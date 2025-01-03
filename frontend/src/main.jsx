1cbdd76005908e3326e5aa3f286c4484396d61d1
edwin yue <53625790+edwaddle@users.noreply.github.com>
2025-01-02 21:41:46 -0800

    Working create page
    Use zusatnd as an API, and use chakra UI for formatting and icons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import {createRoot} from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 
-import { system } from "@chakra-ui/react/preset";
+import {BrowserRouter} from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
-  <ChakraProvider value = {system}>
-    <App/>
+  <ChakraProvider>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </ChakraProvider>
 )
